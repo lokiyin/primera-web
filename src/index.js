18d44b9a1ca35c587f1927d7231f1013ee409b2a
lokiyin <loki.arg@gmail.com>
2021-09-07 23:32:49 -0300

    installe ejs tambien lo agrege al index.js de la raiz

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.listen(3000, () => {
-    console.log("server en el puerto", 3000 );
-})
+const path = require('path');
+const ejs = require('ejs');
+
+//configurations.
+
+
+//routs
+
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, "views/index.html"));
 })
+
+// funcion para escuchar el puerto
+app.listen(3000, () => {
+    console.log("server en el puerto", 3000 );
+});
